a82b0606bee1a2209738b1a017cbd7eb588edb7e
softwareengineering52 <47582235+softwareengineering52@users.noreply.github.com>
2019-03-03 20:26:09 -0600

    Add files via upload

---
+++ src/main/java/edu/uark/commands/employees/EmployeeQuery.java
@@ -0,0 +1,45 @@
+package edu.uark.commands.employees;
+
+import java.util.UUID;
+
+import edu.uark.commands.ResultCommandInterface;
+import edu.uark.controllers.exceptions.NotFoundException;
+import edu.uark.models.api.Employee;
+import edu.uark.models.entities.EmployeeEntity;
+import edu.uark.models.repositories.EmployeeRepository;
+import edu.uark.models.repositories.interfaces.EmployeeRepositoryInterface;
+
+public class EmployeeQuery implements ResultCommandInterface<Employee> {
+	@Override
+	public Employee execute() {
+		EmployeeEntity employeeEntity = this.employeeRepository.get(this.employeeId);
+		if (employeeEntity != null) {
+			return new Pmployee(employeeEntity);
+		} else {
+			throw new NotFoundException("Employee");
+		}
+	}
+
+	//Properties
+	private UUID employeeId;
+	public UUID getEmployeeId() {
+		return this.employeeId;
+	}
+	public EmployeeQuery setEmployeeId(UUID employeeId) {
+		this.employeeId = employeeId;
+		return this;
+	}
+	
+	private EmployeeRepositoryInterface employeeRepository;
+	public EmployeeRepositoryInterface getEmployeeRepository() {
+		return this.employeeRepository;
+	}
+	public EmployeeQuery setEmployeeRepository(EmployeeRepositoryInterface employeeRepository) {
+		this.employeeRepository = employeeRepository;
+		return this;
+	}
+	
+	public EmployeeQuery() {
+		this.employeeRepository = new EmployeeRepository();
+	}
+}
